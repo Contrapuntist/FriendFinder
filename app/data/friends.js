00816469e9ea85026403cbdca986256a92feaac2
Contrapuntist <contrapuntist@gmail.com>
2017-08-06 16:33:25 -0500

    Home page html edits and added bootstrap and jquery cdns

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,5 @@
 
-var friendsData = [
+exports.friendsData = [
     { 
     "name": "Sarah Doe",
     "photo": "http://lorempixel.com/output/people-q-c-250-250-9.jpg",
@@ -15,17 +15,17 @@ var friendsData = [
         5] 
     },
     { 
-    "name": "Sarah Doe",
-    "photo": "http://lorempixel.com/output/people-q-c-250-250-9.jpg",
+    "name": "Igor Gnome",
+    "photo": "http://lorempixel.com/output/people-q-c-250-250-8.jpg",
     "scores": [5, 
         1, 
-        3, 
-        2, 
-        3, 
         4, 
+        2, 
         2, 
         4, 
         1, 
-        5] 
+        5, 
+        1, 
+        1] 
     },
-]
+] 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,16 +1,16 @@
-var friends = require("../data/friends.js");
+var friends = require("../data/friends");
 
 // API routing setup for friends API
-
+console.log (friends.friendsData); 
 module.exports = function(app) {
 
     // GET routing   
     app.get("/api/friends", function(req, res) {
-        res.json(tableData);
+        res.json(friends.friendsData);
     });
 
     app.post("/api/friends", function(req, res) {
-        friendsData.push(req.body);
+        friends.friendsData.push(req.body);
   });
 
 };
