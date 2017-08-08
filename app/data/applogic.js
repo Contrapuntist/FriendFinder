f6dd9db5b651064b6838c36d3d740d91eea5218a
Contrapuntist <contrapuntist@gmail.com>
2017-08-08 13:30:10 -0500

    added script tag to survey html

--- app/data/applogic.js
+++ app/data/applogic.js
@@ -1,6 +1,10 @@
 var friends = require('./friends.js');  
 
 console.log(friends.friendsData);  
+ 
+    $('#submit').on('click', function() {
+        var droptest = $('#q1').val();
+        console.log('value = '+ droptest); 
+    });
 
-
-module.exports = appLogic;
+module.exports = appLogic; 
