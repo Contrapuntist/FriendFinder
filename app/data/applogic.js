27397ed629fb2f118291cdf1ca6b4d3c30c221ee
Contrapuntist <contrapuntist@gmail.com>
2017-08-08 19:47:22 -0500

    adding app logic

--- app/data/applogic.js
+++ app/data/applogic.js
@@ -1,10 +1,19 @@
-var friends = require('./friends.js');  
 
-console.log(friends.friendsData);  
+$(document).ready(function() {
+
+    console.log(friends.friendsData);  
  
     $('#submit').on('click', function() {
         var droptest = $('#q1').val();
         console.log('value = '+ droptest); 
     });
 
-module.exports = appLogic; 
+
+});
+
+
+
+
+
+
+// module.exports = appLogic; 
